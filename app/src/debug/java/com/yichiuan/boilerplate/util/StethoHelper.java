48c1a11872acd7a51aeac9dff6543f78d21a1b3e
yichiuan <yang.yichiuan@gmail.com>
2018-01-05 15:48:37 +0800

    Configure Rxjava 2, Retrofit 2, Gson

--- app/src/debug/java/com/yichiuan/boilerplate/util/StethoHelper.java
+++ app/src/debug/java/com/yichiuan/boilerplate/util/StethoHelper.java
@@ -3,11 +3,18 @@ package com.yichiuan.boilerplate.util;
 import android.content.Context;
 
 import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
 
 
 public class StethoHelper {
 
     public static void init(Context context) {
         Stetho.initializeWithDefaults(context);
+    }
+
+    public static void configureInterceptor(OkHttpClient.Builder httpClientBuilder) {
+        httpClientBuilder.addNetworkInterceptor(new StethoInterceptor());
     }
 }
--- app/src/release/java/com/yichiuan/boilerplate/util/StethoHelper.java
+++ app/src/release/java/com/yichiuan/boilerplate/util/StethoHelper.java
@@ -2,10 +2,16 @@ package com.yichiuan.boilerplate.util;
 
 import android.content.Context;
 
+import okhttp3.OkHttpClient;
+
 
 public class StethoHelper {
 
     public static void init(Context context) {
+
+    }
+
+    public static void configureInterceptor(OkHttpClient.Builder httpClientBuilder) {
 
     }
 }
